b8b6ddaf7f5400b045e16bd96da0347ce1634324
Levi Kanwischer <levi@kanwischer.me>
2015-12-20 21:51:49 -0600

    Rename output argument to outfile

--- legofy/__main__.py
+++ legofy/__main__.py
@@ -23,7 +23,7 @@ from legofy import palettes
 @click.argument('image', required=True, type=click.Path(dir_okay=False,
                                                         exists=True,
                                                         resolve_path=True))
-@click.argument('output', default=None, required=False,
+@click.argument('outfile', default=None, required=False,
                 type=click.Path(resolve_path=True))
 @click.option('--size', default=None, type=int,
               help='Number of bricks the longest side of the legofied image should have.')
@@ -34,8 +34,7 @@ from legofy import palettes
               help='Palette to use based on real Lego colors.')
 def cli(image, output, size, palette, dither):
     """Legofy an image!"""
-    legofy.main(image, outfile=output, size=size,
-                palette=palette, dither=dither)
+    legofy.main(image, outfile, size, palette, dither)
 
 
 if __name__ == '__main__':
